772fd8d5fe522eaa193242b773721b9148373620
shalu2001 <110514943+shalu2001@users.noreply.github.com>
2022-10-10 21:03:34 +0530

    Add files via upload

---
+++ per-website.js
@@ -0,0 +1,30 @@
+
+/*const inputs=document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+inputs.forEach((input) =>{
+    input.addEvenetListener("focus",focusFunc);
+});*/ 
+
+function boxfun(){
+    let test=document.getElementById("paragraph");
+    test.style.color="white";
+    test.style.fontFamily="arial";
+   
+}
+//form validation
+
+/*function myFunction() {
+    let text;
+    let person = prompt("Please enter your name:", "Harry Potter");
+    person.style.color="black";
+    if (person == null || person == "") {
+      text = "User cancelled the prompt.";
+    } else {
+      text = "Hello " + person + "! How are you today?";
+    }
+    document.getElementById("paragraph").innerHTML = text;
+  }*/
